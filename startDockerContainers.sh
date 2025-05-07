d50a80b6e04dce5d0b390fcf25a5b715a7c874cd
Bel9shik <kardash_zapasnoy@mail.ru>
2025-05-07 16:42:21 +0700

    AuthService | docker run script

--- startDockerContainers.sh
+++ startDockerContainers.sh
@@ -1,7 +1,4 @@
 docker start pethost-backend-psql
-sleep 1
 docker start pethost-backend-redis
-sleep 1
 docker start pethost-backend-kafka
-sleep 1
-docker
+docker start pethost-backend-authService
