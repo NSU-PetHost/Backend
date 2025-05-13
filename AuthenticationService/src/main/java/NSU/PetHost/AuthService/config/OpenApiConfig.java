d111532584880efe9c8ab16e76621f96572cc941
Bel9shik <kardash_zapasnoy@mail.ru>
2025-05-13 20:57:34 +0700

    AuthService | работа с gRPC

--- AuthenticationService/src/main/java/NSU/PetHost/AuthService/config/OpenApiConfig.java
+++ AuthenticationService/src/main/java/NSU/PetHost/AuthService/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.persistence.Column;
 
@@ -17,7 +18,8 @@ import jakarta.persistence.Column;
                         email = "a.kardash@g.nsu.ru",
                         url = "https://github.com/Bel9shik"
                 )
-        )
+        ),
+        security = @SecurityRequirement(name = "JWT")
 )
 @SecurityScheme(
         name = "JWT",
