bc4bc9c3920ca819a46af4216920c78bcdaba07b
Bel9shik <kardash_zapasnoy@mail.ru>
2025-05-18 23:01:45 +0700

    ContentService | AnimalsController добавление животных + взаимодействие с фотографиями

--- ContentService/src/main/java/NSU/PetHost/ContentService/repositories/AnimalRepository.java
+++ ContentService/src/main/java/NSU/PetHost/ContentService/repositories/AnimalRepository.java
@@ -5,7 +5,6 @@ import NSU.PetHost.ContentService.models.AnimalsTypes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AnimalRepository extends JpaRepository<Animals, Long> {
 
--- runContentDocker.sh
+++
@@ -1,6 +0,0 @@
-docker network create pethost-net
-docker build -f ContentService/Dockerfile -t pethost-contentservice .
-docker rm -f pethost-backend-contentService
-docker run -d --name pethost-backend-contentService --network pethost-net -p 8081:8081 --mount type=bind,source=$HOME/.m2,target=/root/.m2 pethost-contentservice
-
-echo "ContentService запускается..."
